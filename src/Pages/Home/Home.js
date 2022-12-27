f6e5893ad128982073d80843c414b6178271a3f2
polyakhtar <poly09876pm@gmail.com>
2022-12-27 20:59:06 +0600

    basic setup and navbar designed

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=''>
+            <h2>Hello</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_Mpsf4IkwpGnxAIukySD4DBhUgRja2a4",
+  authDomain: "dream-job-af2da.firebaseapp.com",
+  projectId: "dream-job-af2da",
+  storageBucket: "dream-job-af2da.appspot.com",
+  messagingSenderId: "174749960199",
+  appId: "1:174749960199:web:a8d9080501bb0ef2b102a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
